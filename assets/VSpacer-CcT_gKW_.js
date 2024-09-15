f55363717a94d5a3e53c4ce86d616ba68a0c2178
AndreSecco <andrefigueiro5@gmail.com>
2024-09-15 02:35:43 -0300

    commit alteracoes fotos

---
+++ assets/VSpacer-CcT_gKW_.js
@@ -0,0 +1,1 @@
+import"./VCol-LJ9W6FOU.js";import{c as r}from"./VAvatar-GppZKVst.js";const e=r("v-spacer","div","VSpacer");export{e as V};
---
+++ assets/useRender-DMwfIFM7.js
@@ -0,0 +1,1 @@
+import{p as n,bv as t}from"./index-BFBCBPaS.js";const o=n({class:[String,Array],style:{type:[String,Array,Object],default:null}},"component");function a(e){const r=t("useRender");r.render=e}export{o as m,a as u};
