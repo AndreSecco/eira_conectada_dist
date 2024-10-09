2461032110a500605caec9145b3b7c6308f246a7
AndreSecco <andrefigueiro5@gmail.com>
2024-10-08 21:27:43 -0300

    ajuste dashboard

---
+++ assets/VSpacer-DdCW5hb-.js
@@ -0,0 +1,1 @@
+import"./VCol-CRXqn0_E.js";import{c as r}from"./VAvatar-SC7GiHL5.js";const e=r("v-spacer","div","VSpacer");export{e as V};
---
+++ assets/ssrBoot-OGZottOu.js
@@ -0,0 +1,1 @@
+import{x as t,a2 as e,K as s,bF as n}from"./index-C9yGe_UH.js";function i(){const o=t(!1);return e(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:s(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
