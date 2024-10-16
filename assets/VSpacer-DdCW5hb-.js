e64dfb86eb3b3136e9c7355a4bd58043a1e4e933
AndreSecco <andrefigueiro5@gmail.com>
2024-10-15 22:06:26 -0300

    opcoes em convidados

--- assets/VSpacer-DdCW5hb-.js
+++
@@ -1,1 +0,0 @@
-import"./VCol-CRXqn0_E.js";import{c as r}from"./VAvatar-SC7GiHL5.js";const e=r("v-spacer","div","VSpacer");export{e as V};
---
+++ assets/friends-Cq52g33-.js
@@ -0,0 +1,1 @@
+import{a3 as s,a4 as r}from"./index-CtibLjlr.js";const i=s({id:"Frineds",state:()=>({friends:[]}),getters:{},actions:{async fetchFrineds(){try{const e=await r.get("/api/friends/list");this.friends=e.data.friends}catch(e){alert(e),console.log(e)}}}});export{i as u};
